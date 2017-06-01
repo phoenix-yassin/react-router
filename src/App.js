5a9fd0a97e6dfca243f18a68120e577daab1d73c
yyh1945 <yyh1945@126.com>
2017-06-01 21:34:40 +0800

    1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,99 @@ import {
     Link
 } from 'react-router-dom'
 
+const IMAGES = [
+    { id: 0, title: '深兰花紫', color: 'DarkOrchid' },
+    { id: 1, title: '石灰绿', color: 'LimeGreen' },
+    { id: 2, title: '番茄色', color: 'Tomato' },
+    { id: 3, title: '#七八九', color: '#789' },
+    { id: 4, title: '赤红色', color: 'Crimson'}
+]
+const Home = ()=>(
+    <div>
+        <Link to="/gallery">visit Galary </Link>
+        <h2>精选图片</ h2 >
+        < ul >
+            <li ><Link to="/img/2">番茄色</Link></li >
+            <li ><Link to="/img/4"> 赤红色</Link></ li >
+        </ul >
+    </div>
+)
+
+const Thumbnail = ({color})=>
+    <div style={{width:50,height:50,background:color}}/>
+
+const Gallery = () =>(
+    <div>
+        {IMAGES.map(i => (
+            <Link
+                key={i.id}
+                to={{
+                    pathname: `/img/${i.id}`,
+                    // 这里是关键！
+                    state: { modal: true }
+                }}
+            >
+                <Thumbnail color={i.color} />
+                <p>{i.title}</p>
+            </Link>
+        ))}
+    </div>
+)
+const Image = ({color}) =>
+    <div style={{width:'100%',height:400,background:color}}></div>
+const ImageView = ({match}) => {
+    const image = IMAGES[parseInt(match.params.id, 10)]
+    if (!image) {
+        return <div>找不到图片</div>
+    }
+
+    return (
+        <div>
+            <h1>{image.title}</h1>
+            <Image color={image.color} />
+        </div>
+    )
+}
+const Modal = ({match, history}) => {
+    const image = IMAGES[parseInt(match.params.id, 10)]
+    if (!image) {
+        return null
+    }
+    const back = (e) => {
+        e.stopPropagation()
+        history.goBack()
+    }
+    return (
+        <div
+            onClick={back}
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
+                background: 'rgba(0, 0, 0, 0.15)'
+            }}
+        >
+            <div className='modal' style={{
+                position: 'absolute',
+                background: '#fff',
+                top: 25,
+                left: '10%',
+                right: '10%',
+                padding: 15,
+                border: '2px solid #444'
+            }}>
+                <h1>{image.title}</h1>
+                <Image color={image.color} />
+                <button type='button' onClick={back}>
+                    关闭
+                </button>
+            </div>
+        </div>
+    )
+}
+
 
 // 这个例子展示了如何在相同的 URL 下渲染两个不同的页面（或者在不同 context 下的两个相同页面）。
 // 点击颜色来全屏查看，然后点击「访问Galary」并且在弹窗里查看其他颜色。注意观察弹窗里面
@@ -22,78 +115,37 @@ class ModalSwitch extends React.Component {
     // 「/images/2」这个位置了而Switch会以为当前位置还是「/」。
     preLocation = this.props.location;
     componentWillUpdate(nextProps){
-        // 不是modal，则记录preLocation
-        if(
+        const { location } = this.props
+        // 如果 props.location 不是 modal 的话，就把 this.props.location
+        // 赋值给 previousLocation。
+        if (
             nextProps.history.action !== 'POP' &&
             (!location.state || !location.state.modal)
-        ){
-            this.preLocation = this.props.location;
+        ) {
+            this.preLocation = this.props.location
         }
     }
     render(){
-        const {location} = this.props;
+        const { location } = this.props
         const isModal = !!(
-            location.state && location.state.modal &&
-                this.preLocation !== locaiton
+            location.state &&
+            location.state.modal &&
+            this.preLocation !== location // 不是首次渲染。
         )
         return (
             <div>
-                <Switch location={isModal?this.preLocation:location}>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route path="/gellery" component={Gallery}></Route>
-                    <Route path="img/:id" componet={ImageView}></Route>
+                <Switch location={isModal ? this.preLocation : location}>
+                    <Route exact path='/' component={Home}/>
+                    <Route path='/gallery' component={Gallery}/>
+                    <Route path='/img/:id' component={ImageView}/>
                 </Switch>
-                {isModal?<Route path="img/:id" component={Modal}/> :null}
-
+                {isModal ? <Route path='/img/:id' component={Modal} /> : null}
             </div>
         )
     }
 
 }
-const IMAGES = [
-    { id: 0, title: '深兰花紫', color: 'DarkOrchid' },
-    { id: 1, title: '石灰绿', color: 'LimeGreen' },
-    { id: 2, title: '番茄色', color: 'Tomato' },
-    { id: 3, title: '#七八九', color: '#789' },
-    { id: 4, title: '赤红色', color: 'Crimson'}
-    ]
-const Home = ()=>(
-    <div>
-        <Link to="/gallery">visit Galary </Link>
-        <h2>精选图片</ h2 >
-        < ul >
-            <li ><Link to="/img/2">番茄色</Link></li >
-            <li ><Link to="/img/4"> 赤红色</Link></ li >
-        </ul >
-    </div>
-)
 
-const Thumbnail = ({color})=>
-    <div style={{width:50,height:50,background:color}}/>
-const Gallery = () =>(
-    <div>
-        {IMAGES.map( i => (
-            <Link key={i.id} to={{pathname:`/img/${i.id}`, state:{Modal:true}}}>
-                <Thumbnail color={i.color} />
-                <p>{i.title}</p>
-            </Link>
-        ))}
-    </div>
-)
-const Image = ({color}) =>
-    <div style={{width:'100%',height:400,background:color}}></div>
-const ImageView = ({match}) => {
-    const image = IMAGES[parseInt(match.params.id,10)]
-    if(!iamge){
-        return <div>no images</div>
-    }
-    return (
-        <div>
-            <h1>{image.title}</h1>
-            <Image color={image.color}></Image>
-        </div>
-    )
-}
 const App = () => (
     <Router>
         <Route component={ModalSwitch} />
